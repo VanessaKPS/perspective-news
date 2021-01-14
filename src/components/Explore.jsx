b7a90477e907422bf6092ba1904ca8246c75c4f0
Vanessa Kang <vk@Vanessas-MBP.broadband>
2021-01-14 14:06:13 +0000

    implement react-select for Select component, useEffect and useState hooks to dynamically change values and re-render when user requests new search.

--- src/components/Explore.jsx
+++ src/components/Explore.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react'
 import { getRequestedNews } from './Data'
+import {
+    SortOptions,
+    SearchLanguages,
+    SearchCategories,
+    SearchCountries,
+} from './SearchOptions'
 import '../styles.css'
 const reqSvgs = require.context('../Assets/CountriesSVG', true, /\.svg$/)
 
@@ -14,6 +20,7 @@ const Explore = () => {
     })
     const [reqNewsStories, setReqNewsStories] = useState([])
     const [isClicked, setIsClicked] = useState(false)
+    const [reqNewSearch, setReqNewSearch] = useState(false)
 
     const getRequestedLiveNews = async (searchTerms) => {
         try {
@@ -25,6 +32,7 @@ const Explore = () => {
         }
     }
     const handleClick = () => {
+        // setReqNewSearch((prevValue) => !prevValue)
         setIsClicked((prevValue) => !prevValue)
         setSearchParams({
             keyword: '',
@@ -36,227 +44,158 @@ const Explore = () => {
         })
     }
     const handleSubmit = (e) => {
+        setReqNewSearch((prevValue) => !prevValue)
         setIsClicked((prevValue) => !prevValue)
         getRequestedLiveNews(searchParams)
         e.preventDefault()
     }
 
-    const handleChange = (e) => {
-        // console.log(e.target.name)
-        switch (e.target.name) {
-            case 'keyword':
-                setSearchParams((prevValues) => ({
-                    ...prevValues,
-                    keyword: e.target.value,
-                }))
-                break
-            case 'limit':
-                setSearchParams((prevValues) => ({
-                    ...prevValues,
-                    limit: e.target.value,
-                }))
-                break
+    const handleInputChange = (e) => {
+        if (e.target.name === 'keyword') {
+            setSearchParams((prevValues) => ({
+                ...prevValues,
+                keyword: e.target.value,
+            }))
+        }
+        if (e.target.name === 'limit') {
+            setSearchParams((prevValues) => ({
+                ...prevValues,
+                limit: e.target.value,
+            }))
+        }
+    }
+    const handleSelectChange = (name, option) => {
+        switch (name) {
             case 'sort':
                 setSearchParams((prevValues) => ({
                     ...prevValues,
-                    sort: e.target.value,
-                }))
-                break
-            case 'categories':
-                setSearchParams((prevValues) => ({
-                    ...prevValues,
-                    categories: Array.from(
-                        e.target.selectedOptions,
-                        (option) => option.value
-                    ),
+                    sort: option,
                 }))
 
                 break
+            case 'categories':
+                if (option === null) {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        categories: [],
+                        //
+                    }))
+                } else {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        categories: option,
+                        //
+                    }))
+                }
+
+                break
             case 'countries':
-                setSearchParams((prevValues) => ({
-                    ...prevValues,
-                    countries: Array.from(
-                        e.target.selectedOptions,
-                        (option) => option.value
-                    ),
-                }))
+                if (option === null) {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        countries: [],
+                        //
+                    }))
+                } else {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        countries: option,
+                        //
+                    }))
+                }
                 break
             case 'languages':
-                setSearchParams((prevValues) => ({
-                    ...prevValues,
-                    languages: Array.from(
-                        e.target.selectedOptions,
-                        (option) => option.value
-                    ),
-                }))
+                if (option === null) {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        languages: [],
+                        //
+                    }))
+                } else {
+                    setSearchParams((prevValues) => ({
+                        ...prevValues,
+                        languages: option,
+                        //
+                    }))
+                }
                 break
             default:
                 console.log('Sorry, there is no matching case')
         }
     }
-    const allSvgFilePaths = reqSvgs.keys()
-    const imagePath = allSvgFilePaths[0]
-    const image = reqSvgs(imagePath)
-    console.log(imagePath)
-    console.log(allSvgFilePaths)
-    console.log(image.default)
+
+    // const allSvgFilePaths = reqSvgs.keys()
+    // const imagePath = allSvgFilePaths[0]
+    // const image = reqSvgs(imagePath)
+    // console.log(imagePath)
+    // console.log(allSvgFilePaths)
+    // console.log(image.default)
 
     return (
         <div>
             {/* <img src={image.default} alt='pic' /> */}
             <div className={isClicked ? 'hide' : 'explore-options-form'}>
                 <form onSubmit={handleSubmit}>
-                    <label htmlFor='keyword'>Keywords:</label>
+                    <label htmlFor='keyword'>Keywords</label>
                     <input
+                        className='keyword-input'
                         type='text'
                         id='keyword'
                         name='keyword'
                         value={searchParams.keyword}
-                        onChange={handleChange}
+                        onChange={handleInputChange}
                         placeholder='Separate multiple keywords with ","'
                     ></input>
                     <br />
-                    <label htmlFor='limit'>No. of articles:</label>
+                    <label htmlFor='limit'>No. of articles</label>
                     <input
+                        className='limit-input'
                         type='text'
                         id='limit'
                         name='limit'
                         value={searchParams.limit}
-                        onChange={handleChange}
+                        onChange={handleInputChange}
                         placeholder='Maximum of 100 articles'
                     ></input>
                     <br />
-                    <label htmlFor='sort-select'>Sort by:</label>
-                    <select
+                    <label htmlFor='sort-select'>Sort by</label>
+                    <SortOptions
                         name='sort'
                         id='sort-select'
-                        value={searchParams.sort}
-                        onChange={handleChange}
-                    >
-                        <option value=''>--Please choose an option--</option>
-                        <option value='published_desc'>
-                            Date published (desc)
-                        </option>
-                        <option value='published_asc'>
-                            Date published (asc)
-                        </option>
-                        <option value='popularity'>Popularity</option>
-                    </select>
+                        status={reqNewSearch}
+                        // value={searchParams.sort}
+                        chosen={handleSelectChange}
+                    ></SortOptions>
+
                     <br />
-                    <label htmlFor='categories-select'>
-                        Choose categories:
-                    </label>
-                    <select
+                    <label htmlFor='categories-select'>Categories</label>
+                    <SearchCategories
                         name='categories'
                         id='categories-select'
-                        multiple
-                        value={searchParams.categories}
-                        onChange={handleChange}
-                    >
-                        <option value=''>--Please choose an option--</option>
-                        <option name='business' value='business'>
-                            Business
-                        </option>
-                        <option value='entertainment'>Entertainment</option>
-                        <option value='general'>General</option>
-                        <option value='health'>Health</option>
-                        <option value='science'>Science</option>
-                        <option value='sports'>Sports</option>
-                        <option value='technology'>Tech</option>
-                    </select>
+                        status={reqNewSearch}
+                        // selected={searchParams.categories}
+                        chosen={handleSelectChange}
+                    ></SearchCategories>
                     <br />
-                    <label htmlFor='countries-select'>Choose countries:</label>
-                    <select
+                    <label htmlFor='countries-select'>Countries</label>
+                    <SearchCountries
                         name='countries'
                         id='countries-select'
-                        multiple
-                        value={searchParams.countries}
-                        onChange={handleChange}
-                    >
-                        <option value=''>--Please choose an option--</option>
-                        <option value='ar'>Argentina</option>
-                        <option value='au'>Australia</option>
-                        <option value='at'>Austria</option>
-                        <option value='be'>Belgium</option>
-                        <option value='br'>Brazil</option>
-                        <option value='bg'>Bulgaria</option>
-                        <option value='ca'>Canada</option>
-                        <option value='cn'>China</option>
+                        status={reqNewSearch}
+                        // value={searchParams.countries}
+                        chosen={handleSelectChange}
+                    ></SearchCountries>
 
-                        <option value='co'>Columbia</option>
-                        <option value='cz'>Czech Republic</option>
-                        <option value='eg'>Egypt</option>
-                        <option value='fr'>France</option>
-                        <option value='de'>Germany</option>
-                        <option value='gr'>Greece</option>
-                        <option value='hk'>Hong Kong</option>
-                        <option value='hu'>Hungary</option>
-
-                        <option value='in'>India</option>
-                        <option value='id'>Indonesia</option>
-                        <option value='ie'>Ireland</option>
-                        <option value='il'>Israel</option>
-                        <option value='it'>Italy</option>
-                        <option value='jp'>Japan</option>
-                        <option value='lv'>Latvia</option>
-                        <option value='lt'>Lithuania</option>
-
-                        <option value='my'>Malaysia</option>
-                        <option value='mx'>Mexico</option>
-                        <option value='ma'>Morocco</option>
-                        <option value='nl'>Netherlands</option>
-                        <option value='nz'>New Zealand</option>
-                        <option value='ng'>Nigeria</option>
-                        <option value='no'>Norway</option>
-                        <option value='ph'>Philippines</option>
-
-                        <option value='pl'>Poland</option>
-                        <option value='pt'>Portugal</option>
-                        <option value='ro'>Romania</option>
-                        <option value='sa'>Saudi Arabia</option>
-                        <option value='rs'>Serbia</option>
-                        <option value='sg'>Singapore</option>
-                        <option value='sk'>Slovakia</option>
-                        <option value='si'>Slovenia</option>
-
-                        <option value='za'>South Africa</option>
-                        <option value='kr'>South Korea</option>
-                        <option value='se'>Sweden</option>
-                        <option value='ch'>Switzerland</option>
-                        <option value='tw'>Taiwan</option>
-                        <option value='th'>Thailand</option>
-                        <option value='tr'>Turkey</option>
-                        <option value='ae'>UAE</option>
-
-                        <option value='ua'>Ukraine</option>
-                        <option value='gb'>United Kingdom</option>
-                        <option value='us'>United States</option>
-                        <option value='ve'>Venezuela</option>
-                    </select>
                     <br />
-                    <label htmlFor='languages-select'>Choose languages:</label>
-                    <select
+                    <label htmlFor='languages-select'>Languages</label>
+                    <SearchLanguages
                         name='languages'
                         id='languages-select'
-                        multiple
-                        value={searchParams.languages}
-                        onChange={handleChange}
-                    >
-                        <option value=''>--Please choose an option--</option>
-                        <option value='ar'>Arabic</option>
-                        <option value='zh'>Chinese</option>
-                        <option value='nl'>Dutch</option>
-                        <option value='en'>English</option>
-                        <option value='fr'>French</option>
-                        <option value='de'>German</option>
-                        <option value='he'>Hebrew</option>
-                        <option value='it'>Italian</option>
-                        <option value='no'>Norwegian</option>
-                        <option value='pt'>Portuguese</option>
-                        <option value='ru'>Russian</option>
-                        <option value='es'>Spanish</option>
-                        <option value='sw'>Swedish</option>
-                    </select>
+                        status={reqNewSearch}
+                        // value={searchParams.languages}
+                        chosen={handleSelectChange}
+                    ></SearchLanguages>
+
                     <br />
                     <input type='submit' value='Submit'></input>
                 </form>
@@ -285,7 +224,11 @@ const Explore = () => {
                     </div>
 
                     {searchParams.categories.map((category) => {
-                        return <div className='search-params'>{category}</div>
+                        return (
+                            <div className='search-params' key={category + 1}>
+                                {category}
+                            </div>
+                        )
                     })}
                     {searchParams.countries.map((country) => {
                         return (
@@ -298,7 +241,11 @@ const Explore = () => {
                         )
                     })}
                     {searchParams.languages.map((language) => {
-                        return <div className='search-params'>{language}</div>
+                        return (
+                            <div className='search-params' key={language + 1}>
+                                {language}
+                            </div>
+                        )
                     })}
 
                     <p>Not found what you're looking for? </p>
---
+++ src/components/SearchOptions.jsx
@@ -0,0 +1,128 @@
+import React, { useState, useEffect } from 'react'
+import Select from 'react-select'
+import {
+    sortOptions,
+    categoriesOptions,
+    countriesOptions,
+    languagesOptions,
+} from '../Options'
+
+const SortOptions = (props) => {
+    const [sorOption, setSorOption] = useState([null])
+    useEffect(() => {
+        setSorOption(null)
+    }, [props.status])
+
+    const handleSelect = (item, actionMeta) => {
+        setSorOption(item)
+        let selectedValue = item.value
+
+        props.chosen(actionMeta.name, selectedValue)
+    }
+
+    return (
+        <Select
+            name='sort'
+            options={sortOptions}
+            className='basic-select'
+            classNamePrefix='select'
+            onChange={handleSelect}
+            value={sorOption}
+        />
+    )
+}
+
+const SearchCategories = (props) => {
+    const [catArr, setCatArr] = useState([null])
+    useEffect(() => {
+        setCatArr(null)
+    }, [props.status])
+
+    const handleSelect = (item, actionMeta) => {
+        setCatArr(item)
+        let selectedValues = item
+        if (selectedValues === null) {
+            selectedValues = null
+        } else {
+            selectedValues = Array.from(item, (option) => option.value)
+        }
+
+        props.chosen(actionMeta.name, selectedValues)
+    }
+    return (
+        <Select
+            closeMenuOnSelect={false}
+            isMulti
+            name='categories'
+            options={categoriesOptions}
+            className='basic-multi-select'
+            classNamePrefix='select'
+            onChange={handleSelect}
+            value={catArr}
+        />
+    )
+}
+
+const SearchCountries = (props) => {
+    const [couArr, setCouArr] = useState([null])
+    useEffect(() => {
+        setCouArr(null)
+    }, [props.status])
+
+    const handleSelect = (item, actionMeta) => {
+        setCouArr(item)
+        let selectedValues = item
+        if (selectedValues === null) {
+            selectedValues = null
+        } else {
+            selectedValues = Array.from(item, (option) => option.value)
+        }
+
+        props.chosen(actionMeta.name, selectedValues)
+    }
+
+    return (
+        <Select
+            closeMenuOnSelect={false}
+            isMulti
+            name='countries'
+            options={countriesOptions}
+            className='basic-multi-select'
+            classNamePrefix='select'
+            onChange={handleSelect}
+            value={couArr}
+        />
+    )
+}
+const SearchLanguages = (props) => {
+    const [lanArr, setLanArr] = useState([null])
+    useEffect(() => {
+        setLanArr(null)
+    }, [props.status])
+
+    const handleSelect = (item, actionMeta) => {
+        setLanArr(item)
+        let selectedValues = item
+        if (selectedValues === null) {
+            selectedValues = null
+        } else {
+            selectedValues = Array.from(item, (option) => option.value)
+        }
+
+        props.chosen(actionMeta.name, selectedValues)
+    }
+    return (
+        <Select
+            closeMenuOnSelect={false}
+            isMulti
+            name='languages'
+            options={languagesOptions}
+            className='basic-multi-select'
+            classNamePrefix='select'
+            onChange={handleSelect}
+            value={lanArr}
+        />
+    )
+}
+
+export { SortOptions, SearchCategories, SearchCountries, SearchLanguages }
